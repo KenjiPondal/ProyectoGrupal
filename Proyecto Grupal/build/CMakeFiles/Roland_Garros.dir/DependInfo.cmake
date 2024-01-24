e63a5f697b8bacde584b01684a8d4d499f59f5bd
Pondaljuan <149723241+KenjiPondal@users.noreply.github.com>
2024-01-24 01:57:52 +0100

    new version

---
+++ Proyecto Grupal/build/CMakeFiles/Roland_Garros.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Juan/Desktop/HELL/PEL/Proyecto Grupal/main.cpp" "CMakeFiles/Roland_Garros.dir/main.cpp.obj" "gcc" "CMakeFiles/Roland_Garros.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Proyecto Grupal/build/CMakeFiles/Roland_Garros.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Roland_Garros.dir/main.cpp.obj"
+  "CMakeFiles/Roland_Garros.dir/main.cpp.obj.d"
+  "debug/Roland_Garros.exe"
+  "debug/Roland_Garros.exe.manifest"
+  "debug/Roland_Garros.pdb"
+  "libRoland_Garros.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Roland_Garros.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
